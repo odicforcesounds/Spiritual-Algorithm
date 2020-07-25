1df0fc66c1a44160fdd10b2d0117d3a5abfb6383
rakzhodekams <i00nsu@cryptolab.net>
2020-07-25 18:19:07 +0100

    The hard way: Server sample

---
+++ samples/server.js
@@ -0,0 +1,34 @@
+// Learning the Hard Way
+// Oscar FM
+// 2020
+const fs = require('fs');
+const http = require('http');
+const server http.create.server((res,req) => {
+    const url = req.url;
+    const method = req.method;
+    if (res.url === '/') {
+        res.wrte('<html>')
+        res.wrte('<body>')
+        res.wrte('<h1> OLE </h1>')
+        res.wrte('<form action="message" method="POST">')
+        res.wrte('<input action="/message" name="message">')
+        res.wrte('<body>')
+        res.wrte('</html>')
+    }
+    if (url == '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chuck);
+            body.push(chunk);
+        });
+        req.on('end') () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+            console.log(parsedBody);
+        };
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+});
